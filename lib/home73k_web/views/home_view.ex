eed814a53ad9fb756c9b4bd15befb88dfcf9da79
Adam Piontek <adam@73k.us>
2021-04-06 20:19:49 -0400

    adding noreferrer to social links

--- lib/home73k_web/views/home_view.ex
+++ lib/home73k_web/views/home_view.ex
@@ -13,36 +13,47 @@ defmodule Home73kWeb.HomeView do
       %{
         icon: "mdi-linkedin",
         url: "https://www.linkedin.com/in/adampiontek/",
-        prof: true, target: "_blank"
+        prof: true,
+        target: "_blank"
       },
       %{icon: "mdi-github", url: "https://github.com/apiontek", prof: true, target: "_blank"},
       %{icon: "gitea", url: "https://73k.us/git/adam", prof: true, target: "_blank"},
       %{
         icon: "mdi-key-variant",
         url: Routes.static_path(conn, "/DF185CEE29A3D443_public_key.asc"),
-        prof: true, target: "_blank"
+        prof: true,
+        target: "_blank"
       },
       %{
         icon: "mdi-goodreads",
         url: "https://www.goodreads.com/user/show/2450014-adam-piontek",
-        prof: false, target: "_blank"
+        prof: false,
+        target: "_blank"
       },
-      %{icon: "mdi-twitter", url: "https://twitter.com/adampiontek", prof: false, target: "_blank"},
+      %{
+        icon: "mdi-twitter",
+        url: "https://twitter.com/adampiontek",
+        prof: false,
+        target: "_blank"
+      },
       %{icon: "mdi-facebook", url: "https://facebook.com/damek", prof: false, target: "_blank"},
       %{
         icon: "mdi-instagram",
         url: "https://www.instagram.com/adampiontek/",
-        prof: false, target: "_blank"
+        prof: false,
+        target: "_blank"
       },
       %{
         icon: "mdi-steam",
         url: "https://steamcommunity.com/id/apiontek/",
-        prof: false, target: "_blank"
+        prof: false,
+        target: "_blank"
       },
       %{
         icon: "mdi-discord",
         url: "https://discordapp.com/users/328583977629646848",
-        prof: false, target: "_blank"
+        prof: false,
+        target: "_blank"
       }
     ]
   end
